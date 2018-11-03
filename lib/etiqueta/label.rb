27f56233dd5547a2abefc45ddd5d42dbed1f3cb2
Lucas Alonso <alu0101042305@ull.edu.es>
2018-11-03 11:44:41 +0000

    Creando getters

--- lib/etiqueta/label.rb
+++ lib/etiqueta/label.rb
@@ -1,5 +1,9 @@
 class Label
+
+  attr_reader :nombre, :porcion, :grasas, :grasass, :hc, :azucar, :protei, :sal
+
   def initialize(nombre, porcion, grasas, grasass, hc, azucar, protei, sal)
-    
+    @nombre, @porcion, @grasas, @grasass, @hc, @azucar, @protei, @sal =
+    nombre, porcion, grasas, grasass, hc, azucar, protei, sal
   end
 end
--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -5,9 +5,24 @@ RSpec.describe Etiqueta do
   end
 
   describe Label do
-    @NOMBRE, @PORCION, @GRASAS, @GRASASS,@HC, @AZUCAR, @PROTEI, @SAL =
-    'OREO',    40.0,     7.9,     3.9,   28.0,  15.0,    2.0,   0.35
-    @label = Label.new(@NOMBRE,@PORCION,@GRASAS,@GRASASS,@HC,@AZUCAR,@PROTEI,@SAL)
+
+    before :each do
+      @NOMBRE, @PORCION, @GRASAS, @GRASASS,@HC, @AZUCAR, @PROTEI, @SAL =
+      'OREO',    40.0,     20.0,     9.8,  69.0,  38.0,    5.0,   0.9
+      @label = Label.new(@NOMBRE,@PORCION,@GRASAS,@GRASASS,@HC,@AZUCAR,@PROTEI,@SAL)
+    end
+
+    it "Almacenamiento de los datos" do
+      expect(@label.nombre).to eq(@NOMBRE)
+      expect(@label.porcion).to eq(@PORCION)
+      expect(@label.grasas).to eq(@GRASAS)
+      expect(@label.grasass).to eq(@GRASASS)
+      expect(@label.hc).to eq(@HC)
+      expect(@label.azucar).to eq(@AZUCAR)
+      expect(@label.protei).to eq(@PROTEI)
+      expect(@label.sal).to eq(@SAL)
+    end
+
   end
 
 end
