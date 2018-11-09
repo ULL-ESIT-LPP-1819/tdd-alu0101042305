19e0fe2d7fda674616546a6c1faf9558de339e0a
Lucas Alonso <alu0101042305@ull.edu.es>
2018-11-09 19:48:21 +0000

    Operador <<

--- lib/etiqueta/list.rb
+++ lib/etiqueta/list.rb
@@ -1,3 +1,29 @@
+
+Node = Struct.new(:value, :next, :prev)
+
 class List
+
+  def <<(val)
+    node = Node.new(val)
+    if(@head == nil)
+      @head = node
+      @tail = node
+    else
+      node.prev = @tail
+      @tail.next = node
+      @tail = node
+    end
+    self
+  end
+
+  def to_a
+    array = []
+    node = @head
+    while(node != nil)
+      array << node.value
+      node = node.next
+    end
+    array
+  end
 
 end
